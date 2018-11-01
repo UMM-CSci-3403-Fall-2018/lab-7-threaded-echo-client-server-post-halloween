b2d272618ad82e4b67f24e1e7b60b16130c9b0be
hamer073 <hamer073@morris.umn.edu>
2018-11-01 11:17:22 -0500

    Add classes KeyboardReader and ScreenWriter
    KeyboardReader
    -Takes an OutputStream to send bytes to
    -Reads those bytes from stdin
    
    ScreenWriter
    -Takes an InputStream to read bytes from
    -Writes those bytes to stdout
    
    After the output thread and input thread finish we shutdown the output stream and flush System.out to make sure we don't lose data

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -5,24 +5,77 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 
-public class EchoClient {
+public class EchoClient{
 	public static final int PORT_NUMBER = 6013;
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, InterruptedException {
 		EchoClient client = new EchoClient();
 		client.start();
 	}
 
-	private void start() throws IOException {
+    private class KeyboardReader implements Runnable {
+	    OutputStream stream;
+
+	    public KeyboardReader(OutputStream stream) {
+	       this.stream = stream;
+        }
+
+	    public void run(){
+	        int readByte;
+	        try {
+                while ((readByte = System.in.read()) != -1) {
+                    stream.write(readByte);
+                }
+            } catch(IOException e) {
+                System.out.println("Something went wrong, good luck");
+                System.out.println(e);
+            }
+        }
+
+    }
+
+    private class ScreenWriter implements Runnable {
+	    InputStream stream;
+
+	    public ScreenWriter(InputStream stream) {
+            this.stream = stream;
+        }
+
+	    public void run(){
+	        int inByte;
+	        try {
+                while ((inByte = stream.read()) != -1) {
+                    System.out.write(inByte);
+                }
+            } catch (IOException e) {
+                System.out.println("Whoops");
+                System.out.println(e);
+            }
+
+        }
+
+    }
+
+	private void start() throws IOException, InterruptedException {
 		Socket socket = new Socket("localhost", PORT_NUMBER);
+
 		InputStream socketInputStream = socket.getInputStream();
+		Thread inputThread = new Thread(new ScreenWriter(socketInputStream));
+
 		OutputStream socketOutputStream = socket.getOutputStream();
-		int readByte;
-		while ((readByte = System.in.read()) != -1) {
-			socketOutputStream.write(readByte);
-			int socketByte = socketInputStream.read();
-			System.out.write(socketByte);
-		}
-		System.out.flush();
-	}
+		Thread outputThread = new Thread(new KeyboardReader(socketOutputStream));
+
+		outputThread.start();
+		inputThread.start();
+
+		outputThread.join();
+		socket.shutdownOutput();
+
+
+        inputThread.join();
+
+
+        System.out.flush();
+
+    }
 }
